0de879715511b915fcbd8e1d3102cc3d6de32218
Milan Felix Šulc <rkfelix@gmail.com>
2023-09-28 11:29:18 +0200

    Sanitizer: trim keywords

--- src/sanitizer.ts
+++ src/sanitizer.ts
@@ -3,3 +3,9 @@ import striptags from "striptags";
 export function sanitizeText(str: string): string {
 	return striptags(str);
 }
+
+export function sanitizeArray(str: string | string[]): string[] {
+	return Array.from(Array.isArray(str) ? str : [str]).map((s) =>
+		sanitizeText(s).trim()
+	);
+}
